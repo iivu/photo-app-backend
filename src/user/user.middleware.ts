9f25269d712fc69f89e652933ab4f2223ad26d9f
LEOCHEUNG <leozhang621@gmail.com>
2022-07-27 01:13:56 +0800

    hash user password

--- src/user/user.middleware.ts
+++ src/user/user.middleware.ts
@@ -1,16 +1,21 @@
+import bcrypt from 'bcrypt';
 import type { Request, Response, NextFunction } from 'express';
 
 import * as userService from './user.service';
 
-/**
- * Valide create user data
- */
 export const validateUserData = async (req: Request, res: Response, next: NextFunction) => {
   const { name, password } = req.body;
   if (!name) return next(new Error('Name is required!'));
   if (!password) return next(new Error('Password is required!'));
-  const user = await userService.getUserByName(name);
-  if(user) return next(new Error('User already exist!'))
 
+  const user = await userService.getUserByName(name);
+  if (user) return next(new Error('User already exist!'));
+
+  next();
+};
+
+export const hashUserPassword = async (req: Request, res: Response, next: NextFunction) => {
+  const { password } = req.body;
+  req.body.password = await bcrypt.hash(password, 10);
   next();
 };
--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 
 import * as userController from './user.controller';
-import { validateUserData } from './user.middleware';
+import { validateUserData, hashUserPassword } from './user.middleware';
 
 const router = express.Router();
 
-router.post('/users', validateUserData, userController.create);
+router.post('/users', validateUserData, hashUserPassword, userController.create);
 
 export { router as userRouter };
