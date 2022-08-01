096d4735eacdae184c1a4dd443b7b42294c265a7
LEOCHEUNG <leozhang621@gmail.com>
2022-08-02 01:03:52 +0800

    validate user's login password

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,14 +1,18 @@
+import bcrypt from 'bcrypt'
 import type { Request, Response, NextFunction } from 'express';
 
-import * as userService from '../user/user.service'
+import * as userService from '../user/user.service';
 
 export const validateLoginData = async (req: Request, res: Response, next: NextFunction) => {
-    const { name, password } = req.body;
-    if (!name) return next(new Error('Name is required!'));
-    if (!password) return next(new Error('Password is required!'));
-  
-    const user = await userService.getUserByName(name);
-    if (!user) return next(new Error('User is not exist!'));
-  
-    next();
-  };
+  const { name, password } = req.body;
+  if (!name) return next(new Error('Name is required!'));
+  if (!password) return next(new Error('Password is required!'));
+
+  const user = await userService.getUserByName(name, { password: true });
+  if (!user) return next(new Error('User does not exist!'));
+
+  const passwordMatched = await bcrypt.compare(password,user.password);
+  if(!passwordMatched) return next(new Error('Password does not match!'))
+
+  next();
+};
